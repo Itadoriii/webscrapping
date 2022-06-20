a81b693dbfca4f7e83625d0997f9d4184eb93b8f
Itadoriii <86499855+Itadoriii@users.noreply.github.com>
2022-06-20 16:33:32 -0400

    Update main.py
    xd

--- main.py
+++ main.py
@@ -3,5 +3,5 @@ import pandas as pd
 url = "https://en.wikipedia.org/wiki/History_of_Python"
 dfs = pd.read_html(url)
 df = dfs[0]
-df2 = df[['Version','Release date']]
+df2 = df[['Latest micro version','Release date']]
 print(df2)
